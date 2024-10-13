855737ca00bf3876455da69d5bdf77d68e5b421e
M308 <155259170+citizen-nsl@users.noreply.github.com>
2024-10-14 01:51:24 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+const { createApp, ref, onMounted, reactive } = Vue;
+
+createApp({
+	setup() {
+	  
+	  
+	  onMounted(() => {
+
+		window.addEventListener("message", (event) => {
+
+		});
+  
+	  });
+  
+	  return {
+		// data
+	  };
+	},
+  }).mount("#app");
+  
